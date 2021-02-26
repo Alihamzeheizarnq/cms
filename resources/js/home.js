2083fa84bdb742a2139eeb687880dd58a9392810
alihamzeheizarnq <alihamzehei2017@gmail.com>
2021-02-26 22:25:50 +0330

    update project

--- resources/js/home.js
+++
@@ -1,7 +0,0 @@
-require('./home/jquery');
-require('./home/bootstrap.min');
-require('./admin/demo');
-require('./home/jquery.scrollUp.min');
-require('./home/price-range');
-require('./home/jquery.prettyPhoto');
-require('./home/main');
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/admin.js', 'public/js')
-    .js('resources/js/home.js', 'public/js')
     .sass('resources/css/admin.scss', 'public/css')
-    .sass('resources/css/app.scss', 'public/css')
-    .sass('resources/css/home.scss', 'public/css');
+    .sass('resources/css/app.scss', 'public/css');
